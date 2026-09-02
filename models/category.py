d1c8710e8e34bfb69c1749c7f9182d5eaf1303bf
Vishal <vishalkiranpatel@gmail.com>
2026-09-03 00:06:09 +0530

    added category model

---
+++ models/category.py
@@ -0,0 +1,15 @@
+from extensions import db
+
+class Category(db.Model):
+    __tablename__ = "categories"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+
+    products = db.relationship("Product", backref="category", lazy=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
