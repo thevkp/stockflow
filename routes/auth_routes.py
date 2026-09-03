6d851d3784b9be14403721ed57f684614ce0730f
Vishal <vishalkiranpatel@gmail.com>
2026-09-03 15:41:12 +0530

    No message

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, get_jwt
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, get_jwt # type: ignore
 
 from extensions import db
 from models import User
